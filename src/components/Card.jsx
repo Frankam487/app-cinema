160264307c5b2285da7c47253d7fc67d30cc9007
Frank kamagang <fkamgang06@gmail.com>
2024-07-29 21:09:49 +0200

    img card added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,12 @@ const Card = ({ movie }) => {
     return (
         <div className="card">
             <img src={movie.poster_path ?
-                "https://image.tmdb.org/t/p/original/" + movie.poster_path : movie.poster_path}
-             alt="" />
+                "https://image.tmdb.org/t/p/original/" + movie.poster_path : "./img/poster.jpg"}
+                alt={`affiche ${movie.title}`} />
+            <h2>{movie.title}</h2>
+            {movie.release_date ?
+                <h5>Sorti le : {movie.release_date}</h5> : null
+            }
         </div>
     );
 }
