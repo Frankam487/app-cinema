d958ed6f44c94ea7c8600eb12819b5f577dba3c4
Frank kamagang <fkamgang06@gmail.com>
2024-07-29 21:43:20 +0200

    card file modified

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,90 @@
 const Card = ({ movie }) => {
-    return (
-        <div className="card">
-            <img src={movie.poster_path ?
-                "https://image.tmdb.org/t/p/original/" + movie.poster_path : "./img/poster.jpg"}
-                alt={`affiche ${movie.title}`} />
-            <h2>{movie.title}</h2>
-            {movie.release_date ?
-                <h5>Sorti le : {movie.release_date}</h5> : null
-            }
-        </div>
-    );
+
+    const dateFormater = (date) => {
+        let [yy, mm, dd] = date.split("-");
+        return [dd, mm, yy]
+
+    }
+
+    const genreFinder = () => {
+        let genreArray = [];
+        for (let i = 0; i < movie.genre_ids.length; i++) {
+            switch (movie.genre_ids[i]) {
+                case 28:
+                    genreArray.push(`Action`);
+                    break;
+                case 12:
+                    genreArray.push(`Aventure`);
+                    break;
+                case 16:
+                    genreArray.push(`Animation`);
+                    break;
+                case 35:
+                    genreArray.push(`Comédie`);
+                    break;
+                case 80:
+                    genreArray.push(`Policier`);
+                    break;
+                case 99:
+                    genreArray.push(`Documentaire`);
+                    break;
+                case 18:
+                    genreArray.push(`Drame`);
+                    break;
+                case 10751:
+                    genreArray.push(`Famille`);
+                    break;
+                case 14:
+                    genreArray.push(`Fantasy`);
+                    break;
+                case 36:
+                    genreArray.push(`Histoire`);
+                    break;
+                case 27:
+                    genreArray.push(`Horreur`);
+                    break;
+                case 10402:
+                    genreArray.push(`Musique`);
+                    break;
+                case 9648:
+                    genreArray.push(`Mystère`);
+                    break;
+                case 10749:
+                    genreArray.push(`Romance`);
+                    break;
+                case 878:
+                    genreArray.push(`Science-fiction`);
+                    break;
+                case 10770:
+                    genreArray.push(`Téléfilm`);
+                    break;
+                case 53:
+                    genreArray.push(`Thriller`);
+                    break;
+                case 10752:
+                    genreArray.push(`Guerre`);
+                    break;
+                case 37:
+                    genreArray.push(`Western`);
+                    break;
+                default:
+                    break;
+            
+        }
+    }
+}
+return (
+    <div className="card">
+        <img src={movie.poster_path ?
+            "https://image.tmdb.org/t/p/original/" + movie.poster_path : "./img/poster.jpg"}
+            alt={`affiche ${movie.title}`} />
+        <h2>{movie.title}</h2>
+        {movie.release_date ?
+            <h5>Sorti le : {dateFormater(movie.release_date).join('/')}</h5> : null
+        }
+        <h4>{movie.vote_average}/10 <span>⭐</span></h4>
+    </div>
+);
 }
 
 export default Card;
